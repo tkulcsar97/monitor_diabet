af99669c75ead827da6f88ce7ad55cd873f9c2d8
AlexMihai97 <alexmihaipetrica@gmail.com>
2019-04-19 18:40:33 +0300

    pull

--- mysite/static/js/render(A).js
+++ mysite/static/js/render(A).js
@@ -4,9 +4,11 @@ function renderAG_addValue(){
     document.getElementById("view_addValue").style.display = 'block';
 }
 
-function renderAG_showValues(){
+function renderAG_showValues(message){
     document.getElementById("initial_state").style.display = 'none';
     document.getElementById("view_showValues").style.display = 'block';
+    if (message)
+        document.getElementById("message2").innerHTML = message;
 }
 
 function renderAG_initState(message){
@@ -15,7 +17,23 @@ function renderAG_initState(message){
     document.getElementById("initial_state").style.display = 'block';
 
     if (message){
-        document.getElementById("message").innerHTML = message;
-        document.getElementById("message").style.display = 'block';
+        document.getElementById("message1").innerHTML = message;
+        document.getElementById("message1").style.display = 'block';
     }
+}
+
+function renderRG_addValue(){
+    document.getElementById("initial_state").style.display = 'none';
+    document.getElementById("view_addValue").style.display = 'block';
+}
+
+function renderRG_showValues(){
+    document.getElementById("initial_state").style.display = 'none';
+    document.getElementById("view_showValues").style.display = 'block';
+}
+
+function renderRG_initState(){
+    document.getElementById("view_addValue").style.display = 'none';
+    document.getElementById("view_showValues").style.display = 'none';
+    document.getElementById("initial_state").style.display = 'block';
 }
--- mysite/static/js/server_requests(A).js
+++ mysite/static/js/server_requests(A).js
@@ -12,7 +12,7 @@ function login(){
             window.location.href = "http://localhost:8000/index.htm";
         }
         else{
-            alert("The username or password is incorrect");
+            document.getElementById("error").innerHTML = "The username or password is incorrect";
         }
     }
 
@@ -53,7 +53,55 @@ function logout(){
 }
 
 function adauga_glicemie(){
-    renderAG_initState("Valoarea a fost adaugată cu succes!");
+    var data_valoare = document.getElementById("data_valoare").value;
+    var timp_valoare = document.getElementById("timp_valoare").value;
+    var valoare = document.getElementById("valoare").value;
+    var moment_valoare = new Date(data_valoare + " " + timp_valoare);
+
+    var url = "";
+    var data_to_send = {
+        "moment_valoare": moment_valoare,
+        "valoare": valoare
+    }
+
+    f = function(data_recived){
+        if (data_recived.successful == true)
+            renderAG_initState("Valoarea a fost adaugată cu succes!");
+        else
+            renderAG_initState("A apărut o eroare, incearcă din nou!");
+    }
+    
+}
+
+function preia_glicemie(){
+    var start_date = new Date(document.getElementById("start_date").value);
+    var end_date = new Date(document.getElementById("end_date").value);
+
+    console.log(start_date.getDate());
+    var diffTime = Math.abs(end_date.getTime() - start_date.getTime());
+    var diffDays = Math.ceil(diffTime/(1000 * 60 * 60 * 24));
+
+    if (start_date.getTime() > end_date.getTime() ||  diffDays > 7)
+        renderAG_showValues("Datele nu sunt alese corespunzator");
+    else{
+        var url = "";
+        var data_to_send = {
+            "start_date": start_date.getDate(),
+            "end_date": end_date.getDate()
+        }
+
+        f = function(data_recived){
+
+        }
+    }
+}
+
+function add_RG(){
+
+}
+
+function show_RG(){
+
 }
 
 function ajax_request(url,data,f){
